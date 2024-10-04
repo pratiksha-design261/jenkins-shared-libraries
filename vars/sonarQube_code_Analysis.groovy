98f5560f7ea1b8fec7309dc1bc1f1e640499e6af
pratiksha-design261 <66290960+pratiksha-design261@users.noreply.github.com>
2024-10-05 03:56:05 +0530

    Update sonarQube_code_Analysis.groovy

--- vars/sonarQube_code_Analysis.groovy
+++ vars/sonarQube_code_Analysis.groovy
@@ -1,4 +1,4 @@
-def call(string SonarQubeAPI, string ProjectName, string ProjectKey) {
+def call(String SonarQubeAPI, String ProjectName, String ProjectKey) {
     withSonarQubeEnv("${SonarQubeAPI}"){
         sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${ProjectName} -Dsonar.projectKey=${ProjectKey}"
     }
