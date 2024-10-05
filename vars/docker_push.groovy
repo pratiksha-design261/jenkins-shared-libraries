b7d3e492b30bfe561447d044452f551c6e5f62cc
pratiksha-design261 <66290960+pratiksha-design261@users.noreply.github.com>
2024-10-05 07:56:25 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
-def call(String DockerHubUser, String ProjectName, String ProjectTagName) {
-    withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernamevariable: 'dockerhubuser')]) {
-        sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
-    }
-    sh "docker push ${dockerhubpass}/${ProjectName}:${ProjectTagName}"
+def call(String DockerHubUser, String ProjectName, String ProjectTagName){
+  withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
+      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+  }
+  sh "docker push ${dockerhubuser}/${ProjectName}:${ProjectTagName}"
 }
