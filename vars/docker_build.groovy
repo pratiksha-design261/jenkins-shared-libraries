8f4dce2ac1a16f9e1d9e2998b49daea368b8e60e
pratiksha-design261 <66290960+pratiksha-design261@users.noreply.github.com>
2024-10-05 07:05:48 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,7 +1,3 @@
-//def call(String DockeruserName, String imageName, String tagName){
-   // sh "docker build -t ${DockeruserName}/${imageName}:${tagName} ."
-//}
-// Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(String DockeruserName, String imageName, String tagName){
+    sh "docker build -t ${DockeruserName}/${imageName}:${tagName} ."
 }
